d2a18c1f14db7ffa1eb949676bb82a9a8a896f27
yoshi-gith <0395ckul@gmail.com>
2021-12-26 23:12:14 +0900

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='buttonClass'>
+          <button>プラス</button>
+          <button>マイナス</button>
+          <button>初期化</button>
+        </div>
+        <div className='result'>
+          <p className='text'>結果</p>
+          <p className='count'>5</p>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
