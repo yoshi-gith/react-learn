4acef4953103c104324835e750ff022f1f8a990e
yoshi-gith <0395ckul@gmail.com>
2021-12-29 09:37:55 +0900

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 
 export const App = () => {
+  const [ count, setCount ] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const reduce = () => {
+    setCount(count - 1);
+  };
+  const initial = () => {
+    setCount(count === 0);
+  };
+  
+  
   return (
     <>
       <div className='container'>
         <div className='buttonClass'>
-          <button>プラス</button>
-          <button>マイナス</button>
-          <button>初期化</button>
+          <button onClick={increase}>プラス</button>
+          <button onClick={reduce}>マイナス</button>
+          <button onClick={initial}>初期化</button>
         </div>
         <div className='result'>
           <p className='text'>結果</p>
-          <p className='count'>5</p>
+          <p className='count'>{count}</p>
         </div>
       </div>
     </>
